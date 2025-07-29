3f54d6c292d1a4bd873f588b22b1a5fa9ba5c0eb
dartgorsky <dartgorsky69@gmail.com>
2025-07-29 15:57:43 -0600

    actualizado

--- app_memorygame/forms.py
+++ app_memorygame/forms.py
@@ -1,16 +1,44 @@
+# forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-# Formulario de registro de usuario
 class RegistroForm(UserCreationForm):
     email = forms.EmailField(required=True, label="Correo electrónico")
 
     class Meta:
-        model = User  
-        fields = ['username', 'email', 'password1', 'password2']  # Incluimos campos de contraseña también
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
-# Formulario de inicio de sesión
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Usuario")
-    password = forms.CharField(widget=forms.PasswordInput, label="Contraseña")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Correo electrónico")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
